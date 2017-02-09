6b1e14b97862bc5f1b316c92ddcc42557af58960
slasch <slavaschmidt@gmx.de>
2017-02-09 09:48:02 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,18 @@
+name := "pbt"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+libraryDependencies ++= Seq(
+  "junit" % "junit" % "4.12" % "test",
+
+  "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
+
+  "org.scalactic" %% "scalactic" % "3.0.1",
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test",
+
+  "org.specs2" %% "specs2-core" % "3.8.8" % "test"
+)
+
+scalacOptions in Test ++= Seq("-Yrangepos")
---
+++ src/test/scala/pbt/ScalaTest.scala
@@ -0,0 +1,55 @@
+package pbt
+
+import collection.mutable.Stack
+import org.scalatest._
+import org.scalatest.prop.PropertyChecks
+
+// http://www.scalatest.org/user_guide/property_based_testing
+
+class Fraction(n: Int, d: Int) {
+
+  require(d != 0)
+  require(d != Integer.MIN_VALUE)
+  require(n != Integer.MIN_VALUE)
+
+  val numer = if (d < 0) -1 * n else n
+  val denom = d.abs
+
+  override def toString = numer + " / " + denom
+}
+
+class ScalaTest extends FlatSpec with Matchers with PropertyChecks {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be (2)
+    stack.pop() should be (1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new Stack[Int]
+    a [NoSuchElementException] should be thrownBy {
+      emptyStack.pop()
+    }
+  }
+
+  forAll { (n: Int, d: Int) =>
+
+    whenever (d != 0 && d != Integer.MIN_VALUE
+      && n != Integer.MIN_VALUE) {
+
+      val f = new Fraction(n, d)
+
+      if (n < 0 && d < 0 || n > 0 && d > 0)
+        f.numer should be > 0
+      else if (n != 0)
+        f.numer should be < 0
+      else
+        f.numer should be === 0
+
+      f.denom should be > 0
+    }
+  }
+}
