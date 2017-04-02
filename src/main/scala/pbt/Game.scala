72ffb3dfa983b744b80e056baff809518a358efe
slasch <slavaschmidt@gmx.de>
2017-04-02 12:23:54 +0200

    Another refactor

--- src/main/scala/pbt/Game.scala
+++ src/main/scala/pbt/Game.scala
@@ -10,9 +10,13 @@ object Game {
     type Turn = (Board, Players) => (Board, Players)
     type Strategy = Board => Move
 
+    case class Player(color: Int, strategy: Strategy)
+
     case class Move(x: Int, y: Int, edge: Edge) extends Point {
-      def apply(b: Board, p: Player): Board =
+      def apply(b: Board, p: Player) = {
         b.update(this, p).update(this.adjacent, p)
+      }
+
       lazy val adjacent: Move = edge match {
         case Top => Move(x, y-1, Bottom)
         case Bottom => Move(x, y+1, Top)
@@ -21,24 +25,18 @@ object Game {
       }
     }
 
-    case class Player(color: Int, strategy: Strategy)
-
     case class Board(w: Int, h: Int, cells: Seq[Cell]) extends Rectangle {
-      def this(w: Int, h: Int, cellF: => Cell = new EmptyCell()) =
-        this(w,h, List.fill(w*h)(cellF))
-
+      def this(w: Int, h: Int, cellF: => Cell = new EmptyCell) = this(w,h,List.fill(w*h)(cellF))
       def filter(f: Cell => Boolean) = cells.zipWithIndex.collect {
-        case (c, i) if f(c) => (i % w, i / w, c)
+        case (c, i) if f(c) => (i%w, i/w, c)
       }
-
-      val results = cells.collect {
-        case FullCell(owner) => owner
-      }.groupBy(identity).mapValues(_.length).withDefaultValue(0)
-
-      val winners = results.collect {
-        case (k,v) if v == results.values.max => k
+      lazy val results = cells.collect {
+        case FullCell(p) => p
+      }.groupBy(identity).mapValues(_.length)
+      lazy val gameEnd = results.values.sum == cells.size
+      lazy val winners = results.collect {
+        case (p, count) if count == results.values.max => p
       }.toSet
-
       def update(m: Move, p: Player) = {
         if (contains(m)) {
           val cell = cells(idx(m)).update(m, p)
@@ -50,26 +48,27 @@ object Game {
     sealed trait Cell {
       def owner: Option[Player]
       def edges: Set[Edge]
+      def isEmpty: Boolean
       def update(m: Move, p: Player): Cell
     }
 
     case class EmptyCell(edges: Set[Edge]) extends Cell {
       def this() = this(Set.empty)
       val owner = None
-      def update(m: Move, p: Player) =
+      val isEmpty = true
+      def update(m: Move, p: Player): Cell =
         if (edges.contains(m.edge)) this
         else if (edges.size == 3) FullCell(p)
         else EmptyCell(edges + m.edge)
     }
-
     case class FullCell(player: Player) extends Cell {
-      val owner = Some(player)
+      val owner = Option(player)
       val edges = Edges.all
-      def update(m: Move, p: Player) = this
+      val isEmpty = false
+      def update(m: Move, p: Player): Cell = this
     }
 
-
-  sealed trait Edge
+    sealed trait Edge
     case object Top extends Edge
     case object Bottom extends Edge
     case object Left extends Edge
@@ -79,50 +78,43 @@ object Game {
       val all: Set[Edge] = Set(Top, Bottom, Left, Right)
     }
 
+    def game: Game = (board, players) => game0(board, players, Seq(board))
 
-    val game: Game = (board, players) => game0(board, players, Seq(board))
-
-    val game0: (Board, Players, Log) => (Winners, Log) = (board, players, log) => {
-      if (board.results.values.sum >= board.cells.size) (board.winners, log.reverse)
+    def game0: (Board, Players, Log) => (Winners, Log) = (board, players, log) => {
+      if (board.gameEnd) (board.winners, log)
       else {
         val (nb, np) = turn(board, players)
-        game0(nb, np, nb +: log)
+        game0(nb, np, log :+ nb)
       }
     }
 
-    val turn: Turn = (board, players) => {
+    def turn: Turn = (board, players) => {
       val p = players.head
       val move = p.strategy(board)
-      if (board.cells(board.idx(move)).edges.contains(move.edge)) {
-        new Exception().printStackTrace()
-      }
       val nb = move(board, p)
-      if (nb.results != board.results) (nb, players) else (nb, players.tail :+ players.head)
+      if (nb.results != board.results) (nb, players) else (nb, players.tail :+ p)
     }
 
-
-
-
-  object Strategies {
-    def select(filters: Seq[Cell=>Boolean]): Strategy = board => {
-      filters.foldLeft(None: Option[Move]) { case (result, nextTry) =>
-        result.orElse {
-          board.filter(nextTry) match {
-            case Nil => None
-            case nonempty =>
-              val (x, y, cell) = random(nonempty)
-              val edges = random(Edges.all.diff(cell.edges))
-              Some(Move(x, y, edges))
+    object Strategies {
+      def select(filters: Seq[Cell=>Boolean]): Strategy = board => {
+        filters.foldLeft(None: Option[Move]) { case (result, nextTry) =>
+          result.orElse {
+            board.filter(nextTry) match {
+              case Nil => None
+              case nonempty =>
+                val (x, y, cell) = random(nonempty)
+                Some(Move(x, y, random(Edges.all.diff(cell.edges))))
+            }
           }
-        }
-      }.getOrElse(randomStrategy(board))
-    }
-    val randomStrategy: Strategy = Strategies.select(Seq(_.edges.size < 4))
-    val avoidingStrategy: Strategy = Strategies.select(Seq(_.edges.size < 2))
-    val finishingStrategy: Strategy = Strategies.select(Seq(_.edges.size == 3))
-    val fullStrategy: Strategy = Strategies.select(Seq(_.edges.size == 3, _.edges.size == 1, _.edges.isEmpty))
+        }.getOrElse(randomStrategy(board))
+      }
+      val randomStrategy: Strategy = Strategies.select(Seq(_.edges.size<4))
+      val avoidingStrategy: Strategy = Strategies.select(Seq(_.edges.size < 2))
+      val finishingStrategy: Strategy = Strategies.select(Seq(_.edges.size == 3))
+      val fullStrategy: Strategy = Strategies.select(Seq(_.edges.size == 3, _.edges.size == 1, _.edges.isEmpty))
 
-    val all = Seq(randomStrategy, avoidingStrategy, finishingStrategy, fullStrategy)
-  }
+      val all = Seq(randomStrategy, avoidingStrategy, finishingStrategy, fullStrategy)
+    }
+
 
 }
--- src/test/scala/pbt/GameSpec.scala
+++ src/test/scala/pbt/GameSpec.scala
@@ -13,79 +13,55 @@ object GameSpec extends Properties("Game") {
   } yield Player(color, strategy)
 
   val playersGen = for {
-    num <- Gen.choose(2, 6)
-    players <- Gen.containerOfN[Set, Player](num, playerGen)
-  } yield players.toSeq
-
-  def boardGen(min: Int, max: Int) = for {
-    w <- Gen.choose(min, max)
-    h <- Gen.choose(min, max)
-    n <- Gen.chooseNum(0,3)
-    cellEdges <- Gen.listOfN(w*h, Gen.containerOfN[Set, Edge](n, Gen.oneOf(Edges.all.toSeq)))
-  } yield Board(w,h, cellEdges.map(EmptyCell))
+    num <- Gen.choose(2,6)
+    players <- Gen.listOfN(num, playerGen)
+  } yield players
 
   def emptyBoardGen(min: Int, max: Int) = for {
     w <- Gen.choose(min, max)
     h <- Gen.choose(min, max)
   } yield new Board(w,h)
 
+  def boardGen(min: Int, max: Int) = for {
+    w <- Gen.choose(min, max)
+    h <- Gen.choose(min, max)
+    num <- Gen.choose(0,3)
+    edges <- Gen.listOfN(w*h, Gen.containerOfN[Set, Edge](num, Gen.oneOf(Edges.all.toSeq)))
+  } yield Board(w,h, edges.map(EmptyCell))
+
   def moveGen(b: Board) = for {
-    x <- Gen.choose(1, b.w-2)
-    y <- Gen.choose(1, b.h-2)
-    freeEdge = random(Edges.all.diff(b.cells(y*b.w+x).edges))
-  } yield Move(x,y,freeEdge)
+    w <- Gen.choose(0, b.w)
+    h <- Gen.choose(0, b.h)
+    edge <- Gen.oneOf(Edges.all.toSeq)
+  } yield Move(w,h, edge)
 
-  def borderMoveGen(b: Board) = for {
-    x <- Gen.choose(0, b.w-1)
-    y <- Gen.choose(0, b.h-1)
-    edge = random(Edges.all.diff(b.cells(y*b.w+x).edges))
-  } yield edge match {
-    case Top => Move(x,0,edge)
-    case Bottom => Move(x,b.h-1,edge)
-    case Left => Move(0,y,edge)
-    case Right => Move(b.w-1,y,edge)
-  }
-
-  property("Game") = forAll(boardGen(2, 10), playersGen.suchThat(_.size > 1)) { (b: Board, p: Players) =>
+  property("Game") = forAll(emptyBoardGen(2,20), playersGen.suchThat(_.size>2)) { (b: Board, p: Players) =>
     val (winners, log) = game(b, p)
+    log map SpeakerCheat.printBoard
     all(
-      "all winners played" |: winners.diff(p.toSet).isEmpty,
-      "all cells are taken"|: log.last.filter(_.owner.isEmpty).isEmpty,
-      "winners have most cells"|: winners.forall(log.last.results(_) == log.last.results.values.max)
+      "all winners played a game" |: winners.diff(p.toSet).isEmpty,
+      "no more empty cells" |: log.last.cells.filter(_.isEmpty).isEmpty
     )
   }
 
-  property("Turn") = forAll(boardGen(2, 20), playersGen.suchThat(_.size > 1)) { (b: Board, p: Players) =>
-    val (board, players) = turn(b, p)
-    val cellTaken = board.results(p.head) != b.results(p.head)
-    classify(cellTaken, "cell taken", "no cell taken") {
-      atLeastOne(
-         cellTaken ==> ("player keeps turn if cell was taken" |: players == p),
-         ! cellTaken ==> ("player loses turn if no cell was taken" |: all (
-          players.head == p(1),
-          players.last == p.head
-        ))
-      )
-    }
+  property("Turn") = forAll(boardGen(2,20), playersGen) { (b: Board, p: Players) =>
+    val (nb, np) = turn(b, p)
+    val cellTaken = nb.results != b.results
+    all(
+      "player made a move" |: nb != b,
+      "no more then two cells are changed" |: nb.cells.diff(b.cells).size < 3,
+      classify(cellTaken, "Cell taken", "No cell taken") {
+        atLeastOne(
+          "player keeps the turn if cell was taken" |: cellTaken ==> (p == np),
+          "player changes if no cell was taken" |: !cellTaken ==> (p(1) == np.head && np.last == p.head)
+        )
+      }
+    )
   }
 
-  property("Move") = forAll(emptyBoardGen(4, 20), playerGen) { (b: Board, p: Player) =>
-    forAll (moveGen(b)) { m: Move =>
-      val board = m(b,p)
-      board.cells.diff(b.cells).size == 2
-    }
-  }
-
-  property("Border Move") = forAll(emptyBoardGen(4, 20), playerGen) { (b: Board, p: Player) =>
-    forAll (borderMoveGen(b)) { m: Move =>
-      val board = m(b,p)
-      board.cells.diff(b.cells).size == 1
-    }
-  }
-
-  property("Adjacent Move") = forAll(boardGen(3, 20)) { (b: Board) =>
-    forAll (moveGen(b)) { m: Move =>
-      m == m.adjacent.adjacent
+  property("Move") = forAll(emptyBoardGen(2,20)) { (b: Board) =>
+    forAll(moveGen(b)) { (m: Move) =>
+      m.adjacent.adjacent == m
     }
   }
 
@@ -97,5 +73,4 @@ object GameSpec extends Properties("Game") {
       winningStrategy + 1 >= strategies.max
     }
   }
-
 }
