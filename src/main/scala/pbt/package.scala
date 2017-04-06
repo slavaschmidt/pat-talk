e195070b40d150956f4966f6f22efb0b876e03e8
slasch <slavaschmidt@gmx.de>
2017-04-06 10:32:30 +0200

    Moved filter method to the rectangle

--- src/main/scala/pbt/package.scala
+++ src/main/scala/pbt/package.scala
@@ -11,11 +11,15 @@ package object pbt {
     def y: Int
   }
 
-  trait Rectangle {
+  trait Rectangle[T] {
     def w: Int
     def h: Int
+    def cells: Seq[T]
     def contains(p: Point): Boolean = p.x >= 0 && p.y >= 0 && p.x < w && p.y < h
     def idx(p: Point): Int = p.y * w + p.x
+    def filter(f: T => Boolean) = cells.zipWithIndex.collect {
+      case (c, i) if f(c) => (i%w, i/w, c)
+    }
   }
 
 }
